c7f83161b69e187d4d4ea8b0c60e70487b27636e
Jigar Patel <jpxtel98@outlook.com>
2023-09-18 07:56:19 -0400

    Set up server routes

--- assignment-1/backend/server.js
+++ assignment-1/backend/server.js
@@ -14,19 +14,34 @@ const path = require("path");
 const express = require("express");
 const app = express();
 
-// Add your routes here
-// e.g. app.get() { ... }
+// Define a port to listen to requests on.
+const HTTP_PORT = process.env.PORT || 8080;
 
+// Route handler for the home page ("/")
+app.get("/", (req, res) => {
+    res.send("Welcome to the home page!");
+});
 
+// Route handler for the rentals page ("/rentals")
+app.get("/rentals", (req, res) => {
+    res.send("Browse our listings here.");
+});
 
-// *** DO NOT MODIFY THE LINES BELOW ***
+// Route handler for the registration page ("/sign-up")
+app.get("/sign-up", (req, res) => {
+    res.send("Sign up for our service here.");
+});
+
+// Route handler for the login page ("/log-in")
+app.get("/log-in", (req, res) => {
+    res.send("Log in to your account.");
+});
 
 // This use() will not allow requests to go beyond it
 // so we place it at the end of the file, after the other routes.
 // This function will catch all other requests that don't match
 // any other route handlers declared before it.
 // This means we can use it as a sort of 'catch all' when no route match is found.
-// We use this function to handle 404 requests to pages that are not found.
 app.use((req, res) => {
     res.status(404).send("Page Not Found");
 });
@@ -37,9 +52,6 @@ app.use(function (err, req, res, next) {
     console.error(err.stack)
     res.status(500).send("Something broke!")
 });
-
-// Define a port to listen to requests on.
-const HTTP_PORT = process.env.PORT || 8080;
 
 // Call this function after the http server starts listening for requests.
 function onHttpStart() {
