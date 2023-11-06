35df13f489ce1c416a2570a9b3d21abe36c5f72a
Jigar Patel <jpxtel98@outlook.com>
2023-11-05 19:51:06 -0500

    Add login field required validation

--- server.js
+++ server.js
@@ -1,56 +1,82 @@
 /*************************************************************************************
-* WEB322 - 2237 Project
-* I declare that this assignment is my own work in accordance with the Seneca Academic
-* Policy. No part of this assignment has been copied manually or electronically from
-* any other source (including web sites) or distributed to other students.
-*
-* Student Name  : Jigar Patel
-* Student ID    : 118005172
-* Course/Section: WEB322 NEE
-*
-**************************************************************************************/
+ * WEB322 - 2237 Project
+ * I declare that this assignment is my own work in accordance with the Seneca Academic
+ * Policy. No part of this assignment has been copied manually or electronically from
+ * any other source (including web sites) or distributed to other students.
+ *
+ * Student Name  : Jigar Patel
+ * Student ID    : 118005172
+ * Course/Section: WEB322 NEE
+ *
+ **************************************************************************************/
 
 const path = require("path");
 const rentalsDb = require("./models/rentals-db");
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // Set EJS as the view engine
 app.set("view engine", "ejs");
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
 // Define a port to listen to requests on.
 const HTTP_PORT = process.env.PORT || 8080;
 
 // Route handler for the home page ("/")
 app.get("/", (req, res) => {
-    // Fetch the featured rentals from the rentals-db module
-    const featuredRentals = rentalsDb.getFeaturedRentals();
+  // Fetch the featured rentals from the rentals-db module
+  const featuredRentals = rentalsDb.getFeaturedRentals();
 
-    // Render the 'main.ejs' template and pass the featured rentals / features for home page
-    res.render("main", { content: "home", featuredRentals });
+  // Render the 'main.ejs' template and pass the featured rentals / features for home page
+  res.render("main", { content: "home", featuredRentals });
 });
 
 // Route handler for the rentals page ("/rentals")
 app.get("/rentals", (req, res) => {
-    const allRentals = rentalsDb.getRentalsByCityAndProvince();
+  const allRentals = rentalsDb.getRentalsByCityAndProvince();
 
-    // Render the 'main.ejs' template and pass rental data for the rentals page
-    res.render("main", { content: "rentals", allRentals });
-
+  // Render the 'main.ejs' template and pass rental data for the rentals page
+  res.render("main", { content: "rentals", allRentals });
 });
 
 // Route handler for the registration page ("/sign-up")
 app.get("/signup", (req, res) => {
-    res.render("main", { content: "sign-up" });
+  res.render("main", { content: "sign-up" });
 });
 
-// Route handler for the login page ("/log-in")
 app.get("/login", (req, res) => {
-    res.render("main", { content: "login" });
+    res.render("main", { content: "login", errors: { email: "", password: "" } });
 });
 
+app.post("/login", (req, res) => {
+    const { email, password } = req.body;
+    const validationResult = validateLoginForm(email, password);
+    const errors = validationResult.errors || { email: "", password: "" };
+    res.render("main", { content: "login", errors });
+});
 
+function validateLoginForm(email, password) {
+  const errors = {};
+  // Check if the email is null or empty
+  if (!email || email.trim() === "") {
+    errors.email = "Email cannot be blank";
+  }
+
+  // Check if the password is null or empty
+  if (!password || password.trim() === "") {
+    errors.password = "Password cannot be blank";
+  }
+
+  // Return the validation result
+  return {
+    isValid: Object.keys(errors).length === 0,
+    errors,
+  };
+}
 // *** DO NOT MODIFY THE LINES BELOW ***
 
 // This use() will not allow requests to go beyond it
@@ -59,19 +85,19 @@ app.get("/login", (req, res) => {
 // any other route handlers declared before it.
 // This means we can use it as a sort of 'catch all' when no route match is found.
 app.use((req, res) => {
-    res.status(404).send("Page Not Found");
+  res.status(404).send("Page Not Found");
 });
 
 // This use() will add an error handler function to
 // catch all errors.
 app.use(function (err, req, res, next) {
-    console.error(err.stack)
-    res.status(500).send("Something broke!")
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
 });
 
 // Call this function after the http server starts listening for requests.
 function onHttpStart() {
-    console.log("Express http server listening on: " + HTTP_PORT);
+  console.log("Express http server listening on: " + HTTP_PORT);
 }
 
 // Listen on port 8080. The default port for http is 80, https is 443. We use 8080 here
