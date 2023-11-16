f76e18bdd386f517ff80eb318496f2eac589c7f3
Jigar Patel <jpxtel98@outlook.com>
2023-11-16 13:05:54 -0500

    Start with mongodb connection

---
+++ models/userModel.js
@@ -0,0 +1,26 @@
+/*************************************************************************************
+ * WEB322 - 2237 Project
+ * I declare that this assignment is my own work in accordance with the Seneca Academic
+ * Policy. No part of this assignment has been copied manually or electronically from
+ * any other source (including web sites) or distributed to other students.
+ *
+ * Student Name  : Jigar Patel
+ * Student ID    : 118005172
+ * Course/Section: WEB322 NEE
+ *
+ **************************************************************************************/
+
+const mongoose = require("mongoose");
+
+// Define User Schema
+const userSchema = new mongoose.Schema({
+  fname: { type: String, required: true },
+  lname: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+// Create UserModel based on userSchema
+const UserModel = mongoose.model("User", userSchema);
+
+module.exports = UserModel;
